94e701b5c61b3cfcb95bedda3d90e637fd160792
YasmineHamdy <yasmin22elhosiney@gmail.com>
2026-03-14 09:21:25 +0200

    chore: Initialize Next.js 16 project with TypeScript and Tailwind CSS

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    formats: ["image/avif", "image/webp"],
+    qualities: [60, 75, 80],
+    deviceSizes: [640, 768, 1024, 1280],
+    imageSizes: [96, 128, 200, 256],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+        pathname: "/PokeAPI/sprites/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
